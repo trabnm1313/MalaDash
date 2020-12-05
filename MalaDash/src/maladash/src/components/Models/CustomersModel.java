039249f668c4dc7bd986f199f61abca808404ca7
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2020-12-06 01:19:48 +0700

    Create CustomersModel.java

---
+++ MalaDash/src/maladash/src/components/Models/CustomersModel.java
@@ -0,0 +1,11 @@
+package maladash.src.components.Models;
+
+import maladash.src.components.Classes.Customers;
+
+public class CustomersModel {
+    Customers c1;
+
+    public CustomersModel() {
+        c1 = new Customers(0, false, false, false, 0);
+    } 
+}
