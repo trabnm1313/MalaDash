b767574ecd54173c57511dcda3cca870b79f268d
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2020-12-06 19:12:22 +0700

    Create TrashModel.java

---
+++ MalaDash/src/maladash/src/components/Models/TrashModel.java
@@ -0,0 +1,13 @@
+
+package maladash.src.components.Models;
+
+import maladash.src.components.Classes.Trash;
+
+public class TrashModel {
+    Trash t1;
+
+    public TrashModel() {
+        t1 = new Trash(false);
+    }
+    
+}
